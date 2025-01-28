5c3be3195f1b2a55522aa91c5d342e48155569d8
Periyasamy Palanisamy <pepalani@redhat.com>
2025-01-28 13:49:16 +0100

    Use SHA512WithRSAPSS sig algorithm and key size 3072b to work with future crypto policy
    Signed-off-by: Periyasamy Palanisamy <pepalani@redhat.com>

--- pkg/crypto/crypto.go
+++ pkg/crypto/crypto.go
@@ -428,8 +428,8 @@ const (
 	DefaultCertificateLifetimeDuration   = time.Hour * 24 * 365 * 2 // 2 years
 	DefaultCACertificateLifetimeDuration = time.Hour * 24 * 365 * 5 // 5 years
 
-	// Default keys are 2048 bits
-	keyBits = 2048
+	// Default keys are 3072 bits
+	keyBits = 3072
 )
 
 type CA struct {
@@ -995,7 +995,7 @@ func newSigningCertificateTemplateForDuration(subject pkix.Name, caLifetime time
 	return &x509.Certificate{
 		Subject: subject,
 
-		SignatureAlgorithm: x509.SHA256WithRSA,
+		SignatureAlgorithm: x509.SHA512WithRSAPSS,
 
 		NotBefore: currentTime().Add(-1 * time.Second),
 		NotAfter:  currentTime().Add(caLifetime),
@@ -1033,7 +1033,7 @@ func newServerCertificateTemplateForDuration(subject pkix.Name, hosts []string, 
 	template := &x509.Certificate{
 		Subject: subject,
 
-		SignatureAlgorithm: x509.SHA256WithRSA,
+		SignatureAlgorithm: x509.SHA512WithRSAPSS,
 
 		NotBefore:    currentTime().Add(-1 * time.Second),
 		NotAfter:     currentTime().Add(lifetime),
@@ -1120,7 +1120,7 @@ func NewClientCertificateTemplateForDuration(subject pkix.Name, lifetime time.Du
 	return &x509.Certificate{
 		Subject: subject,
 
-		SignatureAlgorithm: x509.SHA256WithRSA,
+		SignatureAlgorithm: x509.SHA512WithRSAPSS,
 
 		NotBefore:    currentTime().Add(-1 * time.Second),
 		NotAfter:     currentTime().Add(lifetime),
